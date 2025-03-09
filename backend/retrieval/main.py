f7969039eb53cf44fec1eea92bef1a213e4486a6
moreshwarnabar <mrnabar@gmail.com>
2025-03-09 16:37:56 -0700

    added headers and json payload

--- backend/retrieval/main.py
+++ backend/retrieval/main.py
@@ -12,17 +12,23 @@ bedrock_runtime_client = boto3.client('bedrock-agent-runtime', region_name=regio
 def lambda_handler(event, context):
     body = json.loads(event['body'])
 
+    print(f"BODY:\n {body}")
     query = body.get('query')
     msg_history = body.get('msgHistory')
     session_id = body.get('sessionId')
 
     response = retrieve_data(bedrock_runtime_client, msg_history, query)
 
+    payload = {
+        'retrievedData': response,
+        'query': query,
+        'msgHistory': msg_history,
+        'sessionId': session_id
+    }
     return {
         'statusCode': 200,
-        'body': {
-            'retrievedData': response,
-            'query': query,
-            'sessionId': session_id
-        }
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps(payload)
     }
