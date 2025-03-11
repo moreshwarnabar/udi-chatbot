128a113348849ab9561f0ae2c530b07223292ed3
moreshwarnabar <mrnabar@gmail.com>
2025-03-11 14:53:54 -0700

    merged the formatter agent into the generator agent

--- backend/generation/src/generator.py
+++ backend/generation/src/generator.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-
-load_dotenv()
 
 class GeneratorAgent():
     def __init__(self, model: str = "llama-3.3-70b-versatile") -> None:
@@ -12,30 +8,22 @@ class GeneratorAgent():
             ("system", (
                 "You are a seasoned Customer Support Representative. "
                 "You answer customer queries in a live-chat environment. "
-                "Answer the following query based on the provided context. "
+                "Your response should acknowledge the customer's query in a "
+                "natural way. It should use a friendly and approachable tone, "
+                "without being verbose and academic. The response should not exceed"
+                "5 sentences. Answer the following query based on the provided context. "
                 "Context: {context} "
                 "If the context is insufficient, ask for clarification or "
-                "suggest alternative support channels. "
-                "Your response should be short, concise, and professional. "
+                "suggest alternative support channels. Format your response in "
+                "markdown for easy readability using bullet points where necessary. "
             )),
             ("user", "Query: {query}")
-        ])
-        self.formatter_prompt = ChatPromptTemplate.from_messages([
-            ("system", (
-                "You are a seasoned Support Quality Assurance Specialist. "
-                "You check the response from the customer support representative "
-                "to ensure it is short, direct, and professional. "
-                "Ensure the response is clear, concise, and well-structured. "
-                "Query: {query} "
-                "Format it with markdown syntax for easy readability using bullet points "
-                "or spacing where necessary. "
-            )),
-            ("user", "Response: {response}")
         ])
 
     def generate_response(self, query: str, context: str) -> str:
         response = self.llm.invoke(
             self.generator_prompt.format(query=query, context=context)
         )
+        print(f"Response: {response.content}")
 
         return response.content
