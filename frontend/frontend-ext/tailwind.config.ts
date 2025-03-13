c80a27b5c3c6db8f5e428b841bb968559fb686b9
moreshwarnabar <mrnabar@gmail.com>
2025-03-13 16:55:01 -0700

    extended theme by adding custom colors

---
+++ frontend/frontend-ext/tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        asu_maroon: '#8c1d40',
+        asu_gold: '#ffc627',
+        asu_blue: '#00a3e0',
+      },
+    },
+  },
+};
