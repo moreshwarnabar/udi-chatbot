77889fb707b20dfbd3cd6c06fb78e7b625503c12
moreshwarnabar <mrnabar@gmail.com>
2025-03-13 17:03:14 -0700

    added custom colors to theme

--- frontend/frontend-ext/src/components/OpenChatButton.tsx
+++ frontend/frontend-ext/src/components/OpenChatButton.tsx
@@ -7,10 +7,10 @@ interface OpenChatButtonProps {
 const OpenChatButton = ({ onClick }: OpenChatButtonProps) => {
   return (
     <button
-      className="bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 hover:cursor-pointer transition"
+      className="bg-asu_gray text-white p-3 rounded-full shadow-lg hover:bg-asu_blue hover:cursor-pointer transition"
       onClick={onClick}
     >
-      <MessageSquare size={24} />
+      <MessageSquare size={32} />
     </button>
   );
 };
