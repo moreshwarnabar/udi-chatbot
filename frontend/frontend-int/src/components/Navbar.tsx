eb26aa192a144b220239b4a9d1911624e8e59a4e
moreshwarnabar <mrnabar@gmail.com>
2025-03-18 18:18:50 -0700

    updated navbar colors

--- frontend/frontend-int/src/components/Navbar.tsx
+++ frontend/frontend-int/src/components/Navbar.tsx
@@ -8,13 +8,13 @@ interface NavbarProps {
 
 const Navbar = ({ isLoggedIn, loginHandler }: NavbarProps) => {
   return (
-    <nav className="flex justify-between items-center p-3 bg-gray-900 text-white shadow-md">
+    <nav className="flex justify-between items-center p-3 text-asu_maroon shadow-md">
       <img src={logo} alt="asu logo" className="h-10 w-auto" />
-      <div className="text-xl font-bold text-blue-400">UDI Chatbot</div>
+      <div className="text-xl font-bold uppercase">UDI Chatbot</div>
 
       <Button
         onClick={loginHandler}
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 hover:cursor-pointer"
+        className="bg-asu_blue/90 text-white px-4 py-2 rounded-lg hover:bg-asu_blue hover:cursor-pointer"
       >
         {isLoggedIn ? 'Logout' : 'Login'}
       </Button>
